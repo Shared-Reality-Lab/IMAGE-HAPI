d0870bd9f715159940466f0b89dda94647b8d1af
sgannav <sri.ganna@gmail.com>
2021-07-22 21:15:09 -0400

    updated edits

--- JS/Board.js
+++ JS/Board.js
@@ -1,21 +1,89 @@
-function Board(app, portName, baud){
+class Board {
+    communicationType   //type of communication taking place
+    deviceID //ID of device transmitting the information
+    bData //byte inforamation to be transmitted
+    fData //float information to be transmitted
+    type // type of communication taking place
+    expected //number for floating point numbers that are expected
 
+    constructor(portName, baud){
+        serial = new p5.SerialPort();  
+        let options = { baudrate: baud};
+        serial.open(portName, options);              // open a serial port
+        port.clear();
+        reset_board();
+    }
 
+    transmit(communicationType, deviceID, bData, fData){
+        let outData = new Uint8Array[2 + bData.length + 4*fData.length];
+		let segments = new Uint8Array[4];
+		
+		outData[0] = communicationType;
+		outData[1] = deviceID;
+		
+		this.deviceID = deviceID;
+		
+		System.arraycopy(bData, 0, outData, 2, bData.length);
+		
+		let j = 2 + bData.length;
+		for(let i = 0; i < fData.length; i++){
+			segments = FloatToBytes(fData[i]);
+			System.arraycopy(segments, 0, outData, j, 4);
+			j = j + 4;
+		}
+		
+		this.port.write(outData);
+    }
 
-    var reset_board = function(){
+    receive(communicationType, deviceID, expected){
+        set_buffer(1 + 4*expected);
+		
+	    let segments = new Uint8Array[4];
+		
+		let inData = new Uint8Array[1 + 4*expected];
+		let data = new Float32Array[expected];
+		
+		this.port.readBytes(inData);
+		
+		if(inData[0] != deviceID){
+			System.err.println("Error, another device expects this data!");
+		}
+		
+		let j = 1;
+		
+		for(let i = 0; i < expected; i++){
+			System.arraycopy(inData, j, segments, 0, 4);
+			data[i] = BytesToFloat(segments);
+			j = j + 4;
+		}
+		
+		return data;
+    }
+
+    data_available(){
+        available = false;
+        
+        if(port.available() > 0){
+            available = true;
+        }
+        
+        return available;
+    }
+
+    #reset_board = function(){
 		communicationType = 0;
 		deviceID = 0;
-		bData = new byte[0];
-		fData = new float[0];
+		bData = new Uint8Array[0];
+		fData = new Float32Array [0];
 		
 		transmit(communicationType, deviceID, bData, fData);
 	}
 
-    var set_buffer = function(length){
+    #set_buffer = function(length){
 		this.port.buffer(length);
 	}
 
-    var FloatToBytes = function(val){
+    #FloatToBytes = function(val){
   
 		segments = new Uint8Array[4];
   
@@ -30,7 +98,7 @@ function Board(app, portName, baud){
   
 	}
 
-    var BytesToFloat = function(segment){
+    #BytesToFloat = function(segment){
   
 		let temp = 0;
   
@@ -43,61 +111,5 @@ function Board(app, portName, baud){
   
 		return val;
 	}	
-}
-
-Board.prototype.transmit = function(communicationType, deviceID, bData, fData){
-    const outData = new Uint8Array[2 + bData.length + 4*fData.length];
-    const segments = new Uint8Array[4];
     
-    outData[0] = communicationType;
-    outData[1] = deviceID;
-    
-    this.deviceID = deviceID;
-    
-    System.arraycopy(bData, 0, outData, 2, bData.length);
-    
-    let j = 2 + bData.length;
-    for(let i = 0; i < fData.length; i++){
-        segments = FloatToBytes(fData[i]);
-        System.arraycopy(segments, 0, outData, j, 4);
-        j = j + 4;
-    }
-    
-    this.port.write(outData);
-}
-
-Board.prototype.receive = function(communicationType, deviceID, expected){
-		
-    set_buffer(1 + 4*expected);
-    
-    const segments = new Uint8Array[4];
-    
-    const inData = new Uint8Array[1 + 4*expected];
-    const data = new Float32Array[expected];
-    
-    this.port.readBytes(inData);
-    
-    if(inData[0] != deviceID){
-        System.err.println("Error, another device expects this data!");
-    }
-    
-    let  j = 1;
-    
-    for(let i = 0; i < expected; i++){
-        System.arraycopy(inData, j, segments, 0, 4);
-        data[i] = BytesToFloat(segments);
-        j = j + 4;
-    }
-    
-    return data;
-}
-
-Board.prototype.data_available = function(){
-    available = false;
-    
-    if(port.available() > 0){
-        available = true;
-    }
-    
-    return available;
 }
