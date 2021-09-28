58d6565cbff02c175c688abb39693456e3907f42
Yongjae Yoo <yongjae.yoo@mcgill.ca>
2021-09-28 00:59:05 -0400

    YYoo WIP

--- Demoday_Hap101/hap101.js
+++ Demoday_Hap101/hap101.js
@@ -12,10 +12,9 @@ var renderingForce= false;
 var baseFrameRate= 120;
 /* end framerate definition ********************************************************************************************/ 
 
-/* elements definition *************************************************************************************************/
-
 /* Screen and world setup parameters */
 var pixelsPerMeter = 4000.0;
+
 var radsPerDegree = 0.01745;
 
 /* pantagraph link parameters in meters */
@@ -26,73 +25,96 @@ var L = 0.09; // m
 var rEE = 0.006;
 var rEEContact = 0.006;
 
-var posEEToBox;
-var posEEToCentroid;
-
 var jsondata;
 
-var box_ulx;
-var box_uly;
-var box_brx;
-var box_bry;
-
-var screen_ulx;
-var screen_uly;
-var screen_brx;
-var screen_bry;
-
-var angles                              = new p5.Vector(0, 0);
-
 /* task space */
-var posEE                               = new p5.Vector(0, 0);
-var posEELast                           = new p5.Vector(0, 0);
-var fEE                                 = new p5.Vector(0, 0); 
+var angles                             = new p5.Vector (0,0);
+var posEE                              = new p5.Vector(0, 0);
+var posEELast                          = new p5.Vector(0, 0);
+var fEE                                = new p5.Vector(0, 0); 
 
 /* device graphical position */
-var deviceOrigin                        = new p5.Vector(0, 0);
+var deviceOrigin                       = new p5.Vector(0, 0);
 
 /* World boundaries reference */
 const worldPixelWidth                     = 1000;
 const worldPixelHeight                    = 650;
 
+var screenFactor_x = worldPixelWidth/pixelsPerMeter;
+var screenFactor_y = worldPixelHeight/pixelsPerMeter;
+
 /* graphical elements */
 var pGraph, joint, endEffector;
-var leftWall;
-var bottomWall;
-var rightWall;
-var topWall;
+var img, pg;
 
-function onFileLoad()   {
+//Class definition: extracted JSON data, the same structure;
+class DetectedObject{
+  constructor(ID, type, area, centroid, dimensions) {
+    this.ID = ID;
+    this.type = type;
+    this.area = area;
+    this.centroid = centroid;
+    this.dimensions = dimensions;
+  }
+}
+
+class DetectedSegment{
+  constructor(area, centroid, coords, name) {
+    this.area = area;
+    this.centroid = centroid;
+    this.coords = coords;
+    this.name = name;
+  }
+}
+
+var objectdata = [];
+var segmentationdata = [];
+
+//preload
+function preload()  {
+  img = loadImage('image/ex5.jpg');
+}
+
+//loading data from JSON 
+function onFileLoad()   {    
+    //console.log(jsondata);
+    let segments = jsondata.preprocessors['ca.mcgill.a11y.image.preprocessor.semanticSegmentation'].segments;
+    let objects = jsondata.preprocessors['ca.mcgill.a11y.image.preprocessor.objectDetection'].objects;
+    //console.log(objects);
+    console.log(segments);
     
-    console.log(jsondata);
-    //let object = jsondata.object;
-    //let boxcoordinates = object.dimensions;
-    box_ulx = boxcoordinates[0];
-    box_uly = boxcoordinates[1];
-    box_brx = boxcoordinates[2];
-    box_bry = boxcoordinates[3];
-    //console.log(box_ulx);
-    box_ulx = 0;
-    box_uly = 0;
-    box_brx = 0;
-    box_bry = 0;
-    
-    /* converting the box and centroid*/
-    /* -0.1 to 0.1 x, 0 to 0.1 y*/
-    screen_ulx = (box_ulx * 0.25) - 0.125;
-    screen_uly = (box_uly * 0.13);
-    screen_brx = (box_brx * 0.25) - 0.125;
-    screen_bry = (box_bry * 0.13);
+    for (let i = 0; i < objects.length; i++)  {
+        let obj = objects[i];
+        let ID = obj.ID;
+        let area = obj.area;
+        let centroid = obj.centroid;
+        let dimensions = obj.dimensions;
+        let type = obj.type;
+        objectdata.push(new DetectedObject(ID, type, area, centroid, dimensions));
+    }
+    console.log(objectdata);
+    for (let i = 0; i < segments.length; i++)  {
+        let seg = segments[i];
+        let area = seg.area;
+        let centroid = seg.centroid;
+        let coords = seg.coord;
+        let name = seg.nameOfSegment;
+        segmentationdata.push(new DetectedSegment(area, centroid, coords, name));
+    }
+    console.log(segmentationdata);
 }
 
 function setup() {
     createCanvas(1000, 650);
-
+    pg = createGraphics(1000, 650);
     /* visual elements setup */
     //background(255);
     deviceOrigin.add(worldPixelWidth/2, 0);
     
-    jsondata = loadJSON('json/ex5_preprocessor.json', onFileLoad);
+    jsondata = loadJSON('json/ex5_preprocess.json', onFileLoad);
+
+    // loading image
+    image(img, 0, 0);
 
     /* create pantagraph graphics */
     create_pantagraph();
@@ -100,15 +122,13 @@ function setup() {
   }
   
    function draw() {
-
      /* put graphical code here, runs repeatedly at defined framerate in setup, else default at 60fps: */
      //  if(renderingForce == false){
-         background(255);  
-          update_animation(this.angles.x*radsPerDegree, this.angles.y*radsPerDegree, 
+         //background(255);  
+         update_animation(img, pg, this.angles.x*radsPerDegree, this.angles.y*radsPerDegree, 
                             this.posEE.x, this.posEE.y);
      //  }
    }
-
 
 
 async function workerSetup(){
@@ -121,7 +141,6 @@ if (window.Worker) {
     worker = new Worker("hap101_worker.js");
     document.getElementById("button").addEventListener("click", workerSetup);
     worker.addEventListener("message", function(msg){
-
         //retrieve data from worker.js needed for update_animation()
         //TODO: find a more elegant way to retrieve the variables
         angles.x = msg.data[0];
@@ -137,95 +156,136 @@ else {
 
 /* helper functions section, place helper functions here ***************************************************************/
 function create_pantagraph(){
-    var lAni = pixelsPerMeter * l;
-    var LAni = pixelsPerMeter * L;
     var rEEAni = pixelsPerMeter * rEE;
-
-     joint = ellipse(deviceOrigin.x, deviceOrigin.y, rEEAni, rEEAni)
-     joint.beginShape();
-     joint.endShape();
-    
     // endEffector = beginShape(ELLIPSE, deviceOrigin.x, deviceOrigin.y, 2*rEEAni, 2*rEEAni);
   endEffector = ellipse(deviceOrigin.x, deviceOrigin.y, 2*rEEAni, 2*rEEAni)
     
   }
-  
-  
+    
 function create_wall(x1, y1, x2, y2){
-    x1 = pixelsPerMeter * x1;
-    y1 = pixelsPerMeter * y1;
-    x2 = pixelsPerMeter * x2;
-    y2 = pixelsPerMeter * y2;
+    x1 = pixelsPerMeter * x1 * (screenFactor_x);
+    y1 = pixelsPerMeter * y1 * (screenFactor_y);
+    x2 = pixelsPerMeter * x2 * (screenFactor_x);
+    y2 = pixelsPerMeter * y2 * (screenFactor_y);
     
     // return beginShape(LINE, deviceOrigin.x + x1, deviceOrigin.y + y1, deviceOrigin.x + x2, deviceOrigin.y+y2);
-  return line(deviceOrigin.x + x1, deviceOrigin.y + y1, deviceOrigin.x + x2, deviceOrigin.y+y2);
+  return pg.line(x1, y1, x2, y2);
   }
 
-function create_ball(x1, y1, rad)   {
-
+function create_circle(x1, y1, rad)   {
+  x1 = pixelsPerMeter * x1 * (screenFactor_x);
+  y1 = pixelsPerMeter * y1 * (screenFactor_y);
+  rad = pixelsPerMeter * rad * (screenFactor_x);
+  return pg.ellipse(x1, y1, rad, rad);
 }
-  
-function update_animation(th1, th2, xE, yE){
+ 
+function create_rect(x1, y1, x2, y2)   {
+  x1 = pixelsPerMeter * x1 * (screenFactor_x);
+  y1 = pixelsPerMeter * y1 * (screenFactor_y);
+  x2 = pixelsPerMeter * x2 * (screenFactor_x);
+  y2 = pixelsPerMeter * y2 * (screenFactor_y);
+  //console.log (x1, y1, x2, y2);
+  return pg.rect(x1, y1, (x2-x1), (y2-y1));
+}
 
-    /* create left-side wall */
-    leftWall = create_wall(screen_ulx, screen_uly, screen_ulx, screen_bry);
-    leftWall.stroke(color(0));
-    
-    /* create right-sided wall */
-    rightWall = create_wall(screen_brx, screen_uly, screen_brx, screen_bry);
-    rightWall.stroke(color(0));
-    
-    /* create bottom wall */
-    bottomWall = create_wall(screen_ulx, screen_bry, screen_brx, screen_bry);
-    bottomWall.stroke(color(0));
+function update_animation(img, pg, th1, th2, xE, yE){
+    var rec = [];
+    /* object bounding boxes */
+    for (let i = 0; i < objectdata.length; i++) {
+        let ulx = objectdata[i].dimensions[0];
+        let uly = objectdata[i].dimensions[1];
+        let lrx = objectdata[i].dimensions[2];
+        let lry = objectdata[i].dimensions[3];
+        pg.stroke(color(255,0,0));
+        pg.fill(255, 0);
+        rec[i] = create_rect(ulx,uly,lrx,lry);
+        
+        //left
+        //lines[4*i] = create_wall(ulx, uly, ulx, lry);
+        //right
+        //lines[4*i+1] = create_wall(lrx, uly, lrx, lry);
+        //top
+        //lines[4*i+2] = create_wall(ulx, uly, lrx, uly);
+        //bottom
+        //lines[4*i+3] = create_wall(ulx, lry, lrx, lry);        
+    }
+    //pg.fill(255);
+    /* centroids */
+    for (let i = 0; i < segmentationdata.length; i++) {
+      var circs = [];
+      pg.stroke(color(0,0,255));
+      pg.fill(255,255);    
+      let x = segmentationdata[i].centroid[0];
+      let y = segmentationdata[i].centroid[1];
+      circs[i] = create_circle(x, y, 0.02);
+      //console.log(polygons[i]);    
+    }
+    /* semantic segmentation contour */
+    for (let i = 0; i < segmentationdata.length; i++) {
+      var polygons = [];
+      pg.stroke(color(0,255-i*50, i*50)); 
+      pg.fill(255, 0);   
+      polygons[i] = pg.beginShape(LINES);
+      //console.log(segmentationdata[i].coords.length);
+      for (let j = 0; j < segmentationdata[i].coords.length; j++)  {
+        let x = segmentationdata[i].coords[j][0] * pixelsPerMeter * screenFactor_x;
+        let y = segmentationdata[i].coords[j][1] * pixelsPerMeter * screenFactor_y;
 
-    topWall = create_wall(screen_ulx, screen_uly, screen_brx, screen_uly);
-    topWall.stroke(color(0));
+        //console.log(x,y);
+        polygons[i].vertex(x, y);
+      }
+      polygons[i].endShape(CLOSE);
+      //console.log(polygons[i]);    
+    }
 
+    /* End effector */
     th1 = angles.x * (3.14 / 180);
     th2 = angles.y * (3.14 / 180);
     xE = posEE.x;
     yE = posEE.y;
 
-    var lAni = pixelsPerMeter * l;
-    var LAni = pixelsPerMeter * L;
-    
     xE = pixelsPerMeter * -xE;
     yE = pixelsPerMeter * yE;
 
     th1 = 3.14 - th1;
     th2 = 3.14 - th2;
 
-    var lAni = pixelsPerMeter * l;
-    var LAni = pixelsPerMeter * L;
+    //var lAni = pixelsPerMeter * l;
+    //var LAni = pixelsPerMeter * L;
     var rEEAni = pixelsPerMeter * rEE;
 
-    joint = ellipse(deviceOrigin.x, deviceOrigin.y, rEEAni, rEEAni)
-    joint.stroke(color(0));
+    //joint = ellipse(deviceOrigin.x, deviceOrigin.y, rEEAni, rEEAni)
+    //joint.stroke(color(0));
 
-    var v0x = deviceOrigin.x;
-    var v0y = deviceOrigin.y;
-    var v1x = deviceOrigin.x + lAni*cos(th1);
-    var v1y = deviceOrigin.y + lAni*sin(th1);
-    var v2x = deviceOrigin.x + xE;
-    var v2y = deviceOrigin.y + yE;
-    var v3x = deviceOrigin.x + lAni*cos(th2);
-    var v3y = deviceOrigin.y + lAni*sin(th2);
+    //var v0x = deviceOrigin.x;
+    //var v0y = deviceOrigin.y;
+    //var v1x = deviceOrigin.x + lAni*cos(th1);
+    //var v1y = deviceOrigin.y + lAni*sin(th1);
+    //var v2x = deviceOrigin.x + xE;
+    //var v2y = deviceOrigin.y + yE;
+    //var v3x = deviceOrigin.x + lAni*cos(th2);
+    //var v3y = deviceOrigin.y + lAni*sin(th2);
 
    // background(255);
     // p5.js doesn't seem to have setVertex, so the coordinates are set in order rather than using an index 
-    this.pGraph = beginShape();
-     this.pGraph.vertex(v0x, v0y);
-     this.pGraph.vertex(v1x, v1y);
-     this.pGraph.vertex(v2x, v2y);
-     this.pGraph.vertex(v3x, v3y);
+    //this.pGraph = beginShape();
+    // this.pGraph.vertex(v0x, v0y);
+    // this.pGraph.vertex(v1x, v1y);
+    // this.pGraph.vertex(v2x, v2y);
+    // this.pGraph.vertex(v3x, v3y);
     
-    this.pGraph.endShape(CLOSE);
-    translate(xE, yE);
+    //this.pGraph.endShape(CLOSE);
+    //translate(xE, yE);
     
-    endEffector = ellipse(deviceOrigin.x, deviceOrigin.y, 2*rEEAni, 2*rEEAni)
+    pg.stroke(color(64,128,64));
+    pg.fill(255, 0);
+    endEffector = pg.ellipse(deviceOrigin.x, deviceOrigin.y, 2*rEEAni, 2*rEEAni);
     endEffector.beginShape();
     endEffector.endShape();
+    //pg.fill(255);
+
+    image(img, 0,0, 1000, 650);
+    image(pg, 0,0);
   }
   
   
