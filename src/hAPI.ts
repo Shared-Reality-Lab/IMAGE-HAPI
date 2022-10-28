1b5e159ac05ca8f72a94a00dc989d61c48368196
Juliette Regimbal <juliette.regimbal@mail.mcgill.ca>
2022-10-28 16:49:01 -0400

    Add surrounding lib code

---
+++ src/hAPI.ts
@@ -0,0 +1,14 @@
+import { Actuator } from './Actuator';
+import { Board } from './Board';
+import { Device } from './Device';
+import { Pwm } from './Pwm';
+import { Sensor } from './Sensor';
+import { Pantograph as Panto2DIYv1 } from './Pantograph';
+
+export {
+    Actuator,
+    Board,
+    Pwm,
+    Sensor,
+    Panto2DIYv1
+};
