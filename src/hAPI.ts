5f62944bc6040bd5e5bfc34553f050498b6833d3
dafsgit <A01732610@itesm.mx>
2022-11-03 15:38:30 -0600

    new pantograph code

--- src/hAPI.ts
+++ src/hAPI.ts
@@ -4,11 +4,14 @@ import { Device } from './Device';
 import { Pwm } from './Pwm';
 import { Sensor } from './Sensor';
 import { Pantograph as Panto2DIYv1 } from './Pantograph';
+import { NewPantograph as Panto2DIYv3 } from './NewPantograph';
 
 export {
     Actuator,
     Board,
+    Device,
     Pwm,
     Sensor,
-    Panto2DIYv1
+    Panto2DIYv1,
+    Panto2DIYv3
 };
